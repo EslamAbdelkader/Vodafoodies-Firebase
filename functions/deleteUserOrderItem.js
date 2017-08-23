b6217707edac58b961972ed270d9cf99490292ce
Michael Attia <michael.dawood@vodafone.com>
2017-08-23 23:10:06 +0200

    deleteUserOrderItem implementation

--- functions/deleteUserOrderItem.js
+++ functions/deleteUserOrderItem.js
@@ -5,10 +5,24 @@ const admin = require('firebase-admin');
 // Firebase Database Reference
 const database = admin.database();
 
-// COMMENT ON WHAT THE FUNCTION DOES GOES HERE
+// Deletes the item with the id and size provided 
+// from the venue order id provided
 exports.handler = function(req, res) {
     
-    //TODO: implement the function
-    res.status(501).send({"error" : "Method not implemented yet"});
+  // Getting data from the request
+  var userID = req.get("uid");
+  var venueOrderID = req.body.venue_order_id
+  var itemId = req.body.item_id
+  var itemSize = req.body.item_size
+
+  // forming nodes keys
+  var userID_size = userID + "_" + itemSize
+  var itemID_size = itemId + "_" + itemSize
+
+  //Deleting the nodes from the database
+  database.ref("venueOrders/" + venueOrderID + "/itemsSum/" + itemId + "/" + userID_size).remove()
+  database.ref("venueOrders/" + venueOrderID + "/userOrders/" + userID + "/" + itemID_size).remove()
   
+  // responding with success message
+  res.status(200).send("Successfull Operation");
   };
