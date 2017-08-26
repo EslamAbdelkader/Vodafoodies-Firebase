51d7d366ef7c7edda75f337830f29539f36b2508
EslamAbdelkader <Islam.Abdelkader@Vodafone.com>
2017-08-26 09:54:02 +0200

    getVenueData

---
+++ functions/getVenueData.js
@@ -0,0 +1,24 @@
+// The Firebase functions
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+// Firebase Database Reference
+const database = admin.database();
+
+// Returns full data of venue using its id
+exports.handler = functions.https.onRequest((req,res) =>{
+  //The DB references needed
+  var venues = database.ref("venues");
+
+  //Getting data from the request
+  var venue_id = req.body.venue_id;
+
+  //Gettind data from the database
+  venues.child(venue_id).once("value").then(function(data){
+    var venue = data.val();
+    venue.id = venue_id;
+    res.send(venue);
+  }).catch(function(error){
+    console.log(error);
+  });
+});
