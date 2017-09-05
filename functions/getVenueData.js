583a589481b73229d4dac253ee2df88b5287f328
Michael Attia <michael.dawood@vodafone.com>
2017-09-05 21:55:46 +0200

    changing return of venue data to list of items

--- functions/getVenueData.js
+++ functions/getVenueData.js
@@ -16,8 +16,9 @@ exports.handler = functions.https.onRequest((req,res) =>{
   //Gettind data from the database
   venues.child(venue_id).once("value").then(function(data){
     var venue = data.val();
-    venue.id = venue_id;
-    res.send(venue);
+    // venue.id = venue_id;
+    // res.send(venue);
+    res.status(200).send(venue.menu);
   }).catch(function(error){
     console.log(error);
     res.status(500).send("Couldn't Retrieve Venue Data");
